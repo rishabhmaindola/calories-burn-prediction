33c8ebd59f068e5716edb934559d10965692b9f1
Rishabh Maindola <rishabhmaindola5@gmail.com>
2026-06-26 14:32:42 +0530

    fix docker

--- src/lib/ratelimiter.ts
+++ src/lib/ratelimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const globalRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: process.env.NODE_ENV === "production" ? 50 : 50,
+    limit: process.env.NODE_ENV === "production" ? 50 : 10,
 
     standardHeaders: true,
     legacyHeaders: false,
